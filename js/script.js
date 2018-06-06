a0a25ee2173729c37dd126de532926c0e860ca57
Director099 <ferz099@mail.ru>
2018-06-06 14:49:16 +0500

    завершил верстку главной страницы

--- js/script.js
+++ js/script.js
@@ -1,2 +1,25 @@
 'use strict'
 
+
+  var mainContent = document.querySelector('#main-content');
+  var navigation = document.querySelector('.navigation');
+
+  setInterval(function() {
+    if (window.scrollY >= mainContent.offsetTop) {
+      navigation.classList.add('navigation--fixed');
+    } else {
+      navigation.classList.remove('navigation--fixed');
+    }
+  }, 0);
+
+
+
+  var toggleMenu = document.querySelector('.toggle-menu');
+  var mainNav = document.querySelector('.main-nav');
+
+  toggleMenu.addEventListener('click', function() {
+    toggleMenu.classList.toggle('toggle-menu--close');
+    mainNav.classList.toggle('main-nav--open')
+  })
+
+
